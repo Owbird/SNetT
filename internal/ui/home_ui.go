a7e9a418de52b401a5c968f054379fb919cc578f
Owbird <owbirdphorccuo@gmail.com>
2025-10-03 12:53:05 +0000

    feat: Add home ui

--- internal/ui/home_ui.go
+++ internal/ui/home_ui.go
@@ -3,6 +3,9 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"net/url"
 )
 
 type HomeUI struct {
@@ -16,7 +19,26 @@ func NewHomeUI(window fyne.Window) *HomeUI {
 }
 
 func (hui *HomeUI) Home() fyne.CanvasObject {
-	cardsContainer := container.NewGridWithColumns(4)
+	name := widget.NewLabel("SNetT v2.0")
+	name.Alignment = fyne.TextAlignCenter
+	name.TextStyle = fyne.TextStyle{Bold: true}
 
-	return cardsContainer
+	intro := widget.NewLabel("A secure, cross-platform desktop application for managing files over a network.")
+	intro.Wrapping = fyne.TextWrapWord
+	intro.Alignment = fyne.TextAlignCenter
+
+	githubURL, _ := url.Parse("https://github.com/Owbird/SNetT")
+	githubLink := widget.NewHyperlink("View on Github", githubURL)
+	githubLink.Alignment = fyne.TextAlignCenter
+
+	content := container.New(
+		layout.NewVBoxLayout(),
+		layout.NewSpacer(),
+		name,
+		intro,
+		githubLink,
+		layout.NewSpacer(),
+	)
+
+	return content
 }
